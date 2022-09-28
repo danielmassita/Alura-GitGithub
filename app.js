1704a86f0dbb365b6ea935763356b7388b475db3
danielmassita <daniel.massita@gmail.com>
2022-09-28 02:51:36 -0300

    Mantive o palavrao e fazendo a alteração...

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos...")
 console.log("Aplicação está se conectando ao banco de dados...")
 // Agora vou fazer alterações na web em GitHub e vou puxar as alterações no CLI local, após, voltar pra um momento anterior.
 // Se você estiver lendo essa linha "Escrevi e saí correndo, pau no cu de quem tá lendo" é porque não consegui voltar no passado. Se sumiu, aí sim! 
+console.log("Vou manter o humor da piada e da viagem temporal e apenas censurar o palavrão...")
