81718a187155aff10a1996d2069f52c76b2e92f7
Daniel Massita Tonolli <111195175+danielmassita@users.noreply.github.com>
2022-09-28 02:47:55 -0300

    escrevi e sai correndo!

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos...")
 console.log("Aplicação está se conectando ao banco de dados...")
+// Agora vou fazer alterações na web em GitHub e vou puxar as alterações no CLI local, após, voltar pra um momento anterior.
+// Se você estiver lendo essa linha "Escrevi e saí correndo, pau no cu de quem tá lendo" é porque não consegui voltar no passado. Se sumiu, aí sim! 
